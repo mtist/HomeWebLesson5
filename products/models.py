17f7210f3b3a4d819d207dfb74494b8500ddd4dd
mtist <hamza07132@gmail.com>
2018-03-11 01:23:50 +0300

    Пока не все

--- products/models.py
+++ products/models.py
@@ -10,5 +10,12 @@ class Product(models.Model):
     title = models.CharField('Название продукта', max_length=100)
     slug = models.SlugField(max_length=100)
     catalog = models.ForeignKey(Catalog, verbose_name='Каталога', on_delete=models.CASCADE)
+    price = models.IntegerField('Цена продукта')
+    mass = models.IntegerField('Вес продукта')
+    picture = models.ImageField('Фото продукта')
+    manufacturer = models.CharField('Производитель', max_length=100)
     # напишите другие поля: цена вес картинка и тп в зависимости от продукции. Минимум 4 поля
     # какие бывают типы полей можете почитать тут: https://djbook.ru/rel1.9/ref/models/fields.html
+
+    def __str__(self):
+        return self.title
